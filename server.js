5bc19e7a2bc69a3d6963a1346424febf4093a6cb
lessa <lessalsn@gmail.com>
2021-08-23 16:37:13 -0300

    initial commit

---
+++ server.js
@@ -0,0 +1,49 @@
+require('dotenv').config()
+
+const express = require('express')
+const app = express()
+const jwt = require('jsonwebtoken')
+
+app.use(express.json())
+
+const posts = [
+    {
+        username: 'Kyle',
+        title: 'Post 1'
+    },
+    {
+        username: 'Jim',
+        title: 'Post 2'
+    }
+]
+
+app.get('/posts',authenticateToken, (req, res) => {
+    res.send(posts)
+})
+
+app.post('/login', (req, res) => {
+    const username = req.body.username
+    const user = { name: username }
+
+    const accessToken = jwt.sign(user, process.env.REFRESH_TOKEN_SECRET)
+    res.json({ accessToken: accessToken })
+})
+
+function authenticateToken(req, res, next) {
+    const authHeader = req.headers['authorization']
+    const token = authHeader && authHeader.split(' ')[1]
+    console.log(token)
+    if (token == null) return res.sendStatus(401)
+
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
+        console.log(err)
+        if (err) return res.sendStatus(403)
+        req.user = user
+        next()
+    })
+
+}
+
+
+
+app.listen(3000)
