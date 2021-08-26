a08e5a3381773e41cd431855567ee4ea2d3e1fbf
Lessa <lessalsn@gmail.com>
2021-08-26 01:05:40 -0300

    Add refresh token

--- server.js
+++ server.js
@@ -7,43 +7,33 @@ const jwt = require('jsonwebtoken')
 app.use(express.json())
 
 const posts = [
-    {
-        username: 'Kyle',
-        title: 'Post 1'
-    },
-    {
-        username: 'Jim',
-        title: 'Post 2'
-    }
+  {
+    username: 'Kyle',
+    title: 'Post 1'
+  },
+  {
+    username: 'Jim',
+    title: 'Post 2'
+  }
 ]
 
-app.get('/posts',authenticateToken, (req, res) => {
-    res.send(posts)
-})
-
-app.post('/login', (req, res) => {
-    const username = req.body.username
-    const user = { name: username }
-
-    const accessToken = jwt.sign(user, process.env.REFRESH_TOKEN_SECRET)
-    res.json({ accessToken: accessToken })
+app.get('/posts', authenticateToken, (req, res) => {
+  res.json(posts.filter(post => post.username === req.user.name))
 })
 
 function authenticateToken(req, res, next) {
-    const authHeader = req.headers['authorization']
-    const token = authHeader && authHeader.split(' ')[1]
-    console.log(token)
-    if (token == null) return res.sendStatus(401)
+  const authHeader = req.headers['authorization']
+  const token = authHeader && authHeader.split(' ')[1]
+  if (token == null) return res.sendStatus(401)
 
-    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
-        console.log(err)
-        if (err) return res.sendStatus(403)
-        req.user = user
-        next()
-    })
-
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
+    console.log(err)
+    if (err) return res.sendStatus(403)
+    req.user = user
+    next()
+  })
 }
 
-
-
 app.listen(3000)
+
+// https://www.loginradius.com/blog/async/everything-you-want-to-know-about-authorization-headers/
